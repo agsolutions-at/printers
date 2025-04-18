9a7f82864282e5e437540b73dd0164b5e41e3827
Alex Gassner <hello@agsolutions.at>
2025-04-18 14:34:54 +0200

    initial commit

---
+++ __test__/index.spec.mjs
@@ -0,0 +1,8 @@
+import test from 'ava'
+
+import { print } from '../index.js'
+
+test('wrong window handle throw error', (t) => {
+  //const error = t.throws(() => focusWindow(Buffer.from('wrong buffer')))
+  //t.is(error.message, "Invalid HWND buffer size")
+});
---
+++ npm/darwin-x64/README.md
@@ -0,0 +1,3 @@
+# `@agsolutions-at/printers-darwin-x64`
+
+This is the **x86_64-apple-darwin** binary for `@agsolutions-at/printers`
